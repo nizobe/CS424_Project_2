ed47209135c1f8e61e12a49850110ac7fae5c848
Niko <nbessi2@uic.edu>
2014-10-10 16:14:34 -0500

    Commit of everything from Dropbox folder.

---
+++ CS424_Project_2_Mindmap.mm
@@ -0,0 +1,121 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<attribute_registry SHOW_ATTRIBUTES="hide"/>
+<node CREATED="1412709197799" ID="ID_89668344" MODIFIED="1412709850213" TEXT="CS424 Project 2">
+<node CREATED="1412709273001" ID="ID_682735669" MODIFIED="1412733472976" POSITION="right" TEXT="Grade: C">
+<node CREATED="1412709290113" ID="ID_1372248199" MODIFIED="1412709324251" TEXT="Map">
+<node CREATED="1412709338729" ID="ID_858410898" MODIFIED="1412733477431" TEXT="Pan/Zoom Functionality">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1412709357099" ID="ID_1902974595" MODIFIED="1412709370900" TEXT="Satellite Map">
+<node CREATED="1412739443307" ID="ID_1318411003" MODIFIED="1412739451804" TEXT="IF we use Google Maps">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node CREATED="1412709371354" ID="ID_1331142907" MODIFIED="1412733481499" TEXT="&quot;Traditional&quot; Map">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1412709404360" ID="ID_38644242" MODIFIED="1412733485999" TEXT="All Divvy Station Locations">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1412709444402" ID="ID_307220172" MODIFIED="1412712065517" TEXT="Community Areas (as toggle-able layer)"/>
+<node CREATED="1412711618813" ID="ID_1436495796" MODIFIED="1412712335718" TEXT="Google Maps vs. Open Street Map?">
+<icon BUILTIN="help"/>
+</node>
+</node>
+<node CREATED="1412709426587" ID="ID_1937158081" MODIFIED="1412709436797" TEXT="Other">
+<node CREATED="1412709528552" ID="ID_1287628891" MODIFIED="1412709546689" TEXT="Show relative popularity of each station">
+<node CREATED="1412709548565" ID="ID_102411503" MODIFIED="1412712345897" TEXT="Come up w/an arbitrary score?">
+<icon BUILTIN="help"/>
+</node>
+</node>
+<node CREATED="1412709755616" ID="ID_1062672604" MODIFIED="1412709832671" TEXT="animate a day from calendar">
+<node CREATED="1412709774592" ID="ID_1954991244" MODIFIED="1412709786052" TEXT="show all trips taken across city"/>
+<node CREATED="1412709786266" ID="ID_1586615806" MODIFIED="1412709809962" TEXT="link animation to graph showing # active bikes @ time"/>
+</node>
+</node>
+<node CREATED="1412709324965" ID="ID_236929421" MODIFIED="1412709326756" TEXT="Graphs">
+<node CREATED="1412709615059" ID="ID_1287773229" MODIFIED="1412709627190" TEXT="# bikes out by day of week"/>
+<node CREATED="1412709627997" ID="ID_1775982821" MODIFIED="1412709635783" TEXT="# bikes out by hour of day"/>
+<node CREATED="1412709636241" ID="ID_1402527319" MODIFIED="1412709643243" TEXT="# bikes out by day of year"/>
+<node CREATED="1412709643707" ID="ID_218972992" MODIFIED="1412709697019" TEXT="rider demographics">
+<node CREATED="1412709657915" ID="ID_1180384262" MODIFIED="1412709694723" TEXT="male vs. female vs. unknown"/>
+<node CREATED="1412709681398" ID="ID_1680837574" MODIFIED="1412709702765" TEXT="age"/>
+<node CREATED="1412709703084" ID="ID_1164588024" MODIFIED="1412709707591" TEXT="subscriber vs. customer"/>
+</node>
+<node CREATED="1412709713927" ID="ID_354769993" MODIFIED="1412709721103" TEXT="distribution of rides by distance"/>
+<node CREATED="1412709721833" ID="ID_1501750533" MODIFIED="1412709728980" TEXT="distribution of rides by time"/>
+<node CREATED="1412709730042" ID="ID_1013229840" MODIFIED="1412709747224" TEXT="distribution of distance traveled for each bike"/>
+</node>
+</node>
+<node CREATED="1412709851477" ID="ID_109643399" MODIFIED="1412711576441" POSITION="left" TEXT="Grade: B">
+<node CREATED="1412709924294" ID="ID_1777126785" MODIFIED="1412710581284" TEXT="Map">
+<node CREATED="1412710581264" ID="ID_180921735" MODIFIED="1412710583422" TEXT="Stations">
+<node CREATED="1412709987753" HGAP="61" ID="ID_112599168" MODIFIED="1412710512869" TEXT="overall inflow for given station on map" VSHIFT="-22">
+<node CREATED="1412710245699" ID="ID_1642460262" MODIFIED="1412712364966" TEXT="&quot;laurel&quot; arcs on either side showing in/out? ">
+<icon BUILTIN="help"/>
+</node>
+<node CREATED="1412710322065" ID="ID_1882984281" MODIFIED="1412710353352" TEXT="all stations people biked to and # people that biked to each station"/>
+</node>
+<node CREATED="1412710006179" ID="ID_214831556" MODIFIED="1412710215956" TEXT="overall inflow for given station on map">
+<node CREATED="1412710034228" HGAP="17" ID="ID_1232134708" MODIFIED="1412712777326" TEXT="&quot;laurel&quot; arcs on either side showing in/out?" VSHIFT="56">
+<icon BUILTIN="help"/>
+</node>
+<node CREATED="1412710354735" ID="ID_841094576" MODIFIED="1412710371723" TEXT="all stations people biked to and # people that biked to each station"/>
+</node>
+<node CREATED="1412710596743" ID="ID_188351646" MODIFIED="1412710638755" TEXT="Demograhics of riders in/out of station"/>
+<node CREATED="1412710705223" ID="ID_1988990862" MODIFIED="1412710717481" TEXT="Show stations w/biggest imbalance in/out flow @ diff time of day"/>
+</node>
+<node CREATED="1412710726670" ID="ID_301836029" MODIFIED="1412710744777" TEXT="Pick community area and select all stations @ once"/>
+</node>
+<node CREATED="1412709927785" ID="ID_649653373" MODIFIED="1412709929519" TEXT="Other">
+<node CREATED="1412710765460" ID="ID_80545147" MODIFIED="1412710867062" TEXT="playback day from calendar by city or subset of stations">
+<node CREATED="1412710789180" ID="ID_482969024" MODIFIED="1412710797750" TEXT="trips taken"/>
+<node CREATED="1412710797985" ID="ID_1544650302" MODIFIED="1412710823402" TEXT="sunrise/sunset + weather"/>
+<node CREATED="1412710877385" ID="ID_1674647875" MODIFIED="1412710893800" TEXT="grahical data for city as well as each selected station">
+<node CREATED="1412710893800" ID="ID_311882933" MODIFIED="1412710904313" TEXT="filter by gender, age, subscriber vs. customer"/>
+</node>
+</node>
+<node CREATED="1412710915437" ID="ID_613245807" MODIFIED="1412710930944" TEXT="focus on 6 divvy stations my UIC + document observed patterns"/>
+</node>
+<node CREATED="1412709929729" ID="ID_1963236745" MODIFIED="1412709930963" TEXT="Graphs">
+<node CREATED="1412710664993" ID="ID_1539851077" MODIFIED="1412710686371" TEXT="Break day into sections to show patterns for time of day"/>
+</node>
+</node>
+<node CREATED="1412709856551" HGAP="16" ID="ID_210105507" MODIFIED="1412712805313" POSITION="left" STYLE="fork" TEXT="Grade: A" VSHIFT="87">
+<node CREATED="1412710945026" ID="ID_1804238396" MODIFIED="1412711407419" TEXT="Map">
+<node CREATED="1412710947349" ID="ID_610569345" MODIFIED="1412711407419" TEXT="Stations">
+<node CREATED="1412710950202" ID="ID_815497197" MODIFIED="1412711407419" TEXT="pick 2 stations and see info on trips between"/>
+</node>
+<node CREATED="1412710967026" ID="ID_796973959" MODIFIED="1412711407419" TEXT="bring up multiple related maps for various comparisons">
+<node CREATED="1412711001007" ID="ID_1302982161" MODIFIED="1412711407419" TEXT="diff areas of city"/>
+<node CREATED="1412711006501" ID="ID_742641679" MODIFIED="1412711407419" TEXT="diff times of day"/>
+<node CREATED="1412711009850" ID="ID_1078841095" MODIFIED="1412711407419" TEXT="days of week"/>
+<node CREATED="1412711014029" ID="ID_994930688" MODIFIED="1412711407419" TEXT="days of year"/>
+<node CREATED="1412711015999" ID="ID_1814794971" MODIFIED="1412711407419" TEXT="etc."/>
+</node>
+</node>
+<node CREATED="1412711023142" ID="ID_410389313" MODIFIED="1412711407419" TEXT="Other">
+<node CREATED="1412711027763" ID="ID_858535055" MODIFIED="1412711407419" TEXT="document patterns around events in city @ that time">
+<node CREATED="1412711052366" ID="ID_1231098112" MODIFIED="1412711407419" TEXT="school starting"/>
+<node CREATED="1412711057737" ID="ID_721153064" MODIFIED="1412711407419" TEXT="grant park events"/>
+<node CREATED="1412711063456" ID="ID_671534808" MODIFIED="1412711407419" TEXT="games @ soldier field"/>
+<node CREATED="1412711068723" ID="ID_358361431" MODIFIED="1412711407419" TEXT="etc."/>
+</node>
+<node CREATED="1412711076888" ID="ID_284559259" MODIFIED="1412711407419" TEXT="investigate bike usage compared to weather">
+<node CREATED="1412711089817" ID="ID_854233407" MODIFIED="1412711407419" TEXT="temperature"/>
+<node CREATED="1412711096971" ID="ID_940521499" MODIFIED="1412711407419" TEXT="rain"/>
+<node CREATED="1412711098298" ID="ID_1859859078" MODIFIED="1412711407419" TEXT="etc."/>
+</node>
+<node CREATED="1412711102418" ID="ID_1984585550" MODIFIED="1412711407419" TEXT="investigate bike usage compared to sunrise/sunset times"/>
+</node>
+</node>
+<node CREATED="1412711828258" ID="ID_1533495360" MODIFIED="1412711835686" POSITION="right" TEXT="The application should help the user investigate how the bikes are being used, and look for general patterns, and see how particular events (weather related, taste of Chicago, etc) affect those patterns. As with all the projects your app will be evaluated running full screen on the classroom wall.">
+<icon BUILTIN="info"/>
+<node CREATED="1412711785633" ID="ID_662939684" MODIFIED="1412711796271" TEXT="800,000 trips"/>
+<node CREATED="1412711720466" ID="ID_1919744760" MODIFIED="1412711754142" TEXT="187 days of data">
+<node CREATED="1412711754793" ID="ID_406074246" MODIFIED="1412711779363" TEXT="6/27/2013 - 12/31/2014"/>
+</node>
+</node>
+</node>
+</map>
